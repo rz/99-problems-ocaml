a066fafbef99fbd6253c7fedb4202b9a31e21098
Rodrigo Guzman <rodguze@gmail.com>
2016-10-23 09:16:33 -0400

    first stab at using OUnit + Omake for specifying and running a test suite

--- 01-working-with-lists/tests.ml
+++ 01-working-with-lists/tests.ml
@@ -1,13 +1,55 @@
 open OUnit2
 
 
-let test1 tests_ctxt = assert_equal (Some 3) (P01.last [1;2;3]);;
-let test2 tests_ctxt = assert_equal None (P01.last []);;
+let tests_from_specs1 fxn_to_test specs =
+  List.map
+    (fun (label, arg, expected_result) -> (label>:: fun test_ctxt -> assert_equal (fxn_to_test arg) expected_result))
+    specs
+
+(* this is ugly, there has to be a way to say "apply this funciton to this tuple of arguments" *)
+let tests_from_specs2 fxn_to_test specs =
+  List.map
+    (fun (label, (arg1, arg2), expected_result) ->
+      (label>:: fun test_ctxt -> assert_equal (fxn_to_test arg1 arg2) expected_result))
+    specs
+
+
+let p01_tests = tests_from_specs1 P01.last [
+  ("last is last", [1;2;3;4], Some 4);
+  ("last of empty", [], None);
+  ("last of one", [1], Some 1);
+];;
+
+let p02_tests = tests_from_specs1 P02.last_two [
+  ("last 2 are last", [1;2;3;4;5;6], Some (5, 6));
+  ("last 2 of 1", [1], None);
+  ("last 2 of empty", [], None);
+];;
+
+let p03_tests = tests_from_specs2 P03.at [
+  ("at 3 of 6", (3, [1;2;3;4;5;6]), Some 3);
+  ("at 0 of 3", (0, [1;2;3]), None);
+  ("at 1 of 1", (1, [1]), Some 1);
+  ("at 1 of empty", (1, []), None);
+];;
+
+let p04_specs = [
+  ("length of 3", [1;2;3], 3);
+  ("length of 1", [1], 1);
+  ("length of empty", [], 0);
+];;
+
+let p04_tests = (tests_from_specs1 P04.length p04_specs) @ (tests_from_specs1 P04.length_tail p04_specs)
+
+let p05_tests = tests_from_specs1 P05.reverse [
+  ("reverse empty", [], []);
+  ("reverse 1", [1], [1]);
+  ("reverse many", [1;2;3;4;5], [5;4;3;2;1]);
+];;
 
 let suite =
-"suite">:::
-  ["test1">:: test1;
-   "test2">:: test2;]
+  "Working with lists">:::
+    p01_tests @ p02_tests @ p03_tests @ p04_tests @ p05_tests
 ;;
 
 let () =
