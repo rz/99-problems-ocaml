a96a8d21438b766afbfc777afab091f5463b2202
Rodrigo Guzman <rodguze@gmail.com>
2016-11-14 15:14:42 -0500

    improve problem 16 after looking at the solution

--- 01-working-with-lists/p16.ml
+++ 01-working-with-lists/p16.ml
@@ -1,12 +1,24 @@
 (* Drop every N'th element from a list. *)
 
-open Core.Std
+(* my original solution, which kinda sux *)
+(*
+ *open Core.Std
+ *
+ *let rec drop l n =
+ *  match l with
+ *  | [] -> []
+ *  | l -> let fst, lst = List.split_n l n in
+ *    let fl = List.length fst in
+ *    (if fl >= n then List.sub fst ~pos:0 ~len:(fl - 1) else fst) @ drop lst n
+ *;;
+ *)
 
-let rec drop l n =
-  match l with
-  | [] -> []
-  | l -> let fst, lst = List.split_n l n in
-    let fl = List.length fst in
-    (if fl >= n then List.sub fst ~pos:0 ~len:(fl - 1) else fst) @ drop lst n
+(* this is the official solution, which is much better *)
+
+let drop list n =
+  let rec aux i = function
+    | [] -> []
+    | h :: t -> if i = n then aux 1 t else h :: aux (i+1) t in
+  aux 1 list
 ;;
 
